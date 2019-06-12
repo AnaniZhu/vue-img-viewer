5c5a68dcb312ea8adc4eda88aa5ebe18f8abf000
zhuwenhan <zhuwenhan@dxy.cn>
2019-06-12 18:54:10 +0800

    fix: 补充遗漏的iconfont css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ImgPreview from './ImgPreview.vue'
+import '//at.alicdn.com/t/font_1239600_z1ho2s724n.css'
+
 export default {
   install (Vue, options = {}) {
     Vue.Component('ImgPreview', ImgPreview)

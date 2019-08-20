a0feb2f96858e71938bfb8181f6897576ebca4b4
zhuwenhan <zhuwenhan@dxy.cn>
2019-08-20 16:05:26 +0800

    feat: 增加三个对外方法: rotate, zoom, reset. 分别可用来自定义旋转和缩放的数值以及重置状态

--- src/ImagePreview.vue
+++ src/ImagePreview.vue
@@ -27,8 +27,8 @@
           <i class="iconfont icon-actionicon hover-icon" @click="increaseScale" />
           <i class="iconfont icon-suoxiao hover-icon" @click="decreaseScale" />
           <div class="divide" />
-          <i class="iconfont icon-xuanzhuan hover-icon" @click="rotate -= innerAngle" />
-          <i class="iconfont icon-xuanzhuan-r hover-icon" @click="rotate += innerAngle" />
+          <i class="iconfont icon-xuanzhuan hover-icon" @click="rotateAngle -= innerAngle" />
+          <i class="iconfont icon-xuanzhuan-r hover-icon" @click="rotateAngle += innerAngle" />
         </div>
         <transition name="fade">
           <div
@@ -53,6 +53,8 @@ const DEFAULT_ANGLE = 90
 const BASE_SELECTOR = 'img' // 默认选择器
 const DEFAULT_FILTER_FUNCTION = () => true // 插槽模式下，默认过滤函数
 
+const ALERT = text => console.log(`Error in vue-img-viewer: ${text}`)
+
 export default {
   name: 'ImagePreview',
   props: {
@@ -75,7 +77,7 @@ export default {
       default: DEFAULT_MAX_SCALE,
       validator (val) {
         let result = Number.isFinite(+val)
-        if (!result) console.error('prop maxScale 必须为Number类型或者数字字符串')
+        if (!result) ALERT('prop maxScale 必须为Number类型或者数字字符串')
         return result
       }
     },
@@ -84,7 +86,7 @@ export default {
       default: DEFAULT_MIN_SCALE,
       validator (val) {
         let result = Number.isFinite(+val)
-        if (!result) console.error('prop minScale 必须为Number类型或者数字字符串')
+        if (!result) ALERT('prop minScale 必须为Number类型或者数字字符串')
         return result
       }
     },
@@ -92,7 +94,7 @@ export default {
       type: [String, Number],
       validator (val) {
         let result = Number.isFinite(+val)
-        if (!result) console.error('prop scaleStep 必须为Number类型或者数字字符串')
+        if (!result) ALERT('prop scaleStep 必须为Number类型或者数字字符串')
         return result
       },
       default: DEFAULT_STEP
@@ -101,7 +103,7 @@ export default {
       type: [String, Number],
       validator (val) {
         let result = Number.isFinite(+val)
-        if (!result) console.error('prop angle 必须为Number类型或者数字字符串')
+        if (!result) ALERT('prop angle 必须为Number类型或者数字字符串')
         return result
       },
       default: DEFAULT_ANGLE
@@ -131,7 +133,7 @@ export default {
       imgList: [],
       urlList: [],
       scale: 1,
-      rotate: 0,
+      rotateAngle: 0,
       aspectRatio: 1,
       position: {
         left: 0,
@@ -177,7 +179,7 @@ export default {
       let { left, top } = this.position
       let styleKey = this.aspectRatio > 1 ? 'max-width' : 'max-height'
       return {
-        transform: `translate3d(${left}px, ${top}px, 0) scale(${this.scale}) rotate(${this.rotate}deg)`,
+        transform: `translate3d(${left}px, ${top}px, 0) scale(${this.scale}) rotate(${this.rotateAngle}deg)`,
         [styleKey]: '100%'
       }
     },
@@ -248,6 +250,44 @@ export default {
     }
   },
   methods: {
+    // ==================================== 对外方法 Start =============================================
+    rotate (angle) {
+      if (typeof angle === 'function') {
+        angle = +angle(this.rotateAngle)
+      } else {
+        angle = +angle
+      }
+      if (Number.isFinite(angle)) {
+        this.rotateAngle = angle
+      } else {
+        ALERT('rotate方法参数必须为一个数字或函数(如果是函数，则为函数的返回值必须为数字)')
+      }
+    },
+    zoom (zoomRate) {
+      if (typeof zoomRate === 'function') {
+        zoomRate = +zoomRate(this.scale)
+      } else {
+        zoomRate = +zoomRate
+      }
+
+      if (Number.isFinite(zoomRate)) {
+        // 限制缩放范围在“设定范围之内”
+        if (zoomRate < this.minScale) {
+          this.scale = this.minScale
+        } else if (zoomRate > this.maxScale) {
+          this.scale = this.maxScale
+        } else {
+          this.scale = zoomRate
+        }
+        // console.error(`zoom传入的参数(如果是函数，则为函数的返回值)超过设定的缩放范围，该范围为${this.minScale}~${this.maxScale}`)
+      } else {
+        ALERT('zoom方法参数必须为一个数字或函数(如果是函数，则为函数的返回值必须为数字)')
+      }
+    },
+    reset () {
+      return this.resetImage()
+    },
+    // ==================================== 对外方法 End =============================================
     /**
      * 根据传入的css选择器筛选，生成最终的选择器
      * 举个例子
@@ -296,7 +336,7 @@ export default {
     },
     resetImage () {
       this.scale = 1
-      this.rotate = 0
+      this.rotateAngle = 0
       this.position = {
         left: 0,
         top: 0
@@ -328,14 +368,10 @@ export default {
       this.resetImage()
     },
     increaseScale () {
-      if (this.scale < this.innerMaxScale) {
-        this.scale = +(this.scale + this.innerScaleStep).toFixed(1) // 处理精度丢失
-      }
+      this.zoom(scale => (scale + this.innerScaleStep).toFixed(1))// 处理精度丢失
     },
     decreaseScale () {
-      if (this.scale > this.innerMinScale) {
-        this.scale = +(this.scale - this.innerScaleStep).toFixed(1) // 处理精度丢失
-      }
+      this.zoom(scale => (scale - this.innerScaleStep).toFixed(1)) // 处理精度丢失
     },
     // 图片拽拉
     handleImageMouseDown (e) {

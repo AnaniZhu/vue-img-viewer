4a930453b512d1ec8dd2ccb134170de5f1ae2431
zhuwenhan <zhuwenhan@dxy.cn>
2020-02-16 01:59:51 +0800

    chore: git commit message 允许版本号

--- scripts/commitmsg.validate.js
+++ scripts/commitmsg.validate.js
@@ -3,9 +3,11 @@ const fs = require('fs')
 const commitMsg = fs.readFileSync(process.env.HUSKY_GIT_PARAMS, 'utf-8')
 const commitPattern = /^(feat|fix|improvement|docs|style|refactor|pref|test|build|ci|chore|revert):\s[\s\S]+$/g
 
-if (!commitPattern.test(commitMsg)) {
+const versionPattern = /^\d\.\d\.\d\n*$/
+
+if (!commitPattern.test(commitMsg) && !versionPattern.test(commitMsg)) {
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(' 💅 sorry, its a invalid commit message format.')}\n\n` +
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(` 💅 sorry, its a invalid commit message format: ${commitMsg}`)}\n\n` +
     chalk.red(' please see under examples:\n\n') +
     `     👉  commit message: ${chalk.green('feat: 新增xx功能')}\n`
   )
